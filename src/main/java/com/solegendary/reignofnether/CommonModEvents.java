908bd3ee3801569a8e6eb49856e84a7f2bdfadf4
ThatBusyBoi <supermccrafter25@gmail.com>
2025-12-20 21:01:57 -0400

    Temporarily disable /rts-scores + incomplete gui
    PostMatchScoresGui and PostMatchScoresGuiContainer are the new classes that handle the creation and logic for the rts-scores screen. However, due to some struggles and complaints, its best to temporarily disable the playerless version of /rts-scores.

--- src/main/java/com/solegendary/reignofnether/CommonModEvents.java
+++ src/main/java/com/solegendary/reignofnether/CommonModEvents.java
@@ -7,6 +7,7 @@ import com.solegendary.reignofnether.building.buildings.placements.PortalPlaceme
 import com.solegendary.reignofnether.entities.models.NecromancerProjectileModel;
 import com.solegendary.reignofnether.entities.renderers.ThrowableTntRenderer;
 import com.solegendary.reignofnether.entities.renderers.NecromancerProjectileRenderer;
+import com.solegendary.reignofnether.guiscreen.PostMatchScoresGui;
 import com.solegendary.reignofnether.guiscreen.TopdownGui;
 import com.solegendary.reignofnether.registrars.*;
 import com.solegendary.reignofnether.unit.modelling.models.*;
@@ -169,6 +170,7 @@ public class CommonModEvents {
     @SubscribeEvent
     public static void onClientSetupEvent(FMLClientSetupEvent evt) {
         evt.enqueueWork(() -> MenuScreens.register(ContainerRegistrar.TOPDOWNGUI_CONTAINER.get(), TopdownGui::new));
+        evt.enqueueWork(() -> MenuScreens.register(ContainerRegistrar.POSTMATCHSCORESGUI_CONTAINER.get(), PostMatchScoresGui::new));
     }
 
     @SubscribeEvent
--- src/main/java/com/solegendary/reignofnether/registrars/ContainerRegistrar.java
+++ src/main/java/com/solegendary/reignofnether/registrars/ContainerRegistrar.java
@@ -1,8 +1,10 @@
 package com.solegendary.reignofnether.registrars;
 
 import com.solegendary.reignofnether.ReignOfNether;
+import com.solegendary.reignofnether.guiscreen.PostMatchScoresGuiContainer;
 import com.solegendary.reignofnether.guiscreen.TopdownGuiContainer;
 import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +18,9 @@ public class ContainerRegistrar {
 
     public static final RegistryObject<MenuType<TopdownGuiContainer>> TOPDOWNGUI_CONTAINER = CONTAINERS
             .register("topdowngui_container", () -> new MenuType<>(TopdownGuiContainer::new, FeatureFlagSet.of()));
+
+    public static final RegistryObject<MenuType<PostMatchScoresGuiContainer>> POSTMATCHSCORESGUI_CONTAINER = CONTAINERS
+            .register("postmatchscoresgui_container", () -> new MenuType<>(PostMatchScoresGuiContainer::new, FeatureFlags.DEFAULT_FLAGS));
 
     public static void init(FMLJavaModLoadingContext context) {
         CONTAINERS.register(context.getModEventBus());
